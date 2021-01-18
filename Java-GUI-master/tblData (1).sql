a09e7b57f0b509b9b444fcdd0617bd9e3bbc868a
dianynf <dianbugas@gmail.com>
2021-01-18 21:47:28 +0700

    ok

--- Java-GUI-master/tblData (1).sql
+++ Java-GUI-master/tblData (1).sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Waktu pembuatan: 18 Jan 2021 pada 19.19
--- Versi server: 8.0.22-0ubuntu0.20.04.3
--- Versi PHP: 7.4.13
+-- Host: localhost
+-- Waktu pembuatan: 18 Jan 2021 pada 15.47
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Karyawan`
+-- Database: `karyawan`
 --
 
 -- --------------------------------------------------------
@@ -29,21 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tblData` (
-  `ID` int NOT NULL,
+  `ID` int(11) NOT NULL,
   `Nama` varchar(30) NOT NULL,
-  `Jenis Kelamin` varchar(1) NOT NULL,
   `Kota` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data untuk tabel `tblData`
 --
 
-INSERT INTO `tblData` (`ID`, `Nama`, `Jenis Kelamin`, `Kota`) VALUES
-(1, 'Abdul', 'L', 'Bandung'),
-(2, 'Siti', 'P', 'Jakarta'),
-(3, 'Tejo', 'P', 'Jogjakarta'),
-(4, 'Jarwo', 'L', 'Solo');
+INSERT INTO `tblData` (`ID`, `Nama`, `Kota`) VALUES
+(1, 'Abdul', 'Bandung'),
+(2, 'Jarwo', 'Solo'),
+(3, 'Siti', 'Jakarta'),
+(4, 'Tejo', 'Jogjakarta');
 
 --
 -- Indexes for dumped tables
